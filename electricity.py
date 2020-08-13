cd077331c2212e66a01167c8997131a38f5676f7
arnaud <arnaud.mercier@orange.com>
2020-08-13 19:43:17 +0200

    Fixed Procfile

--- electricity.py
+++ electricity.py
@@ -12,5 +12,5 @@ def hello():
     else:
         return render_template('hello.html')
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=True,host='0.0.0.0')
