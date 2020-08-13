01501a7dbcacb4aa5f7a882a5a0ab4368ac8add2
arnaud <arnaud.mercier@orange.com>
2020-08-13 15:55:14 +0200

    added a second page

--- electricity.py
+++ electricity.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'This is the index page'
+
+@app.route('/hello')
+def hello():
+    return 'This is the hello page'
