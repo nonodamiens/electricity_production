c75e5bf07a018855b7aca21d9c8090b514320856
arnaud <arnaud.mercier@orange.com>
2020-08-13 17:52:39 +0200

    Added request method and POST

--- electricity.py
+++ electricity.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/hello')
+@app.route('/hello', methods=["POST"])
 def hello():
-    return 'This is the hello page'
+    if request.method == "POST":
+        return 'Bonjour vous avez mis comme pseudo {} et mdp {}'.format(request.form['pseudo'], request.form['pass'])
+    else:
+        return 'This is the hello page'
