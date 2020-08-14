d30bf810e091ae94f74ab273d1e34916f355c85e
arnaud <arnaud.mercier@orange.com>
2020-08-14 15:12:12 +0200

    Added DB init code

--- electricity.py
+++ electricity.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request
+from models import db
 
 app = Flask(__name__)
+
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'password',
+    'db': 'my_database',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+db.init_app(app)
 
 @app.route('/')
 def index():
