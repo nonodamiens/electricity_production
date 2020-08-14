5685c1ac0a2bb34abdc5a94e66a1970f75015bd6
arnaud <arnaud.mercier@orange.com>
2020-08-14 15:02:47 +0200

    Added a models file

--- electricity.py
+++ electricity.py
@@ -1,8 +1,4 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-
-# Initialize a database object
-db = SQLAlchemy()
 
 app = Flask(__name__)
 
---
+++ models.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize a database object
+db = SQLAlchemy()
