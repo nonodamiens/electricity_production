9ed5ea08ecabe36e752860e7530eb709d81b12d6
arnaud <arnaud.mercier@orange.com>
2020-09-10 15:30:01 +0200

    change pyodbc for pypyodbc

--- electricity.py
+++ electricity.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-import pyodbc
+import pypyodbc
 import urllib
 import os
 
