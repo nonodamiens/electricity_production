01083b635e37cb064cd15d13e262d38b43949e27
arnaud <arnaud.mercier@orange.com>
2020-09-14 20:03:57 +0200

    added elements to deploy on azure

--- electricity.py
+++ electricity.py
@@ -18,7 +18,7 @@ params = urllib.parse.quote_plus \
 (r'Driver='+driver+';Server=tcp:'+server+',1433;Database='+database+';Uid='+username+';Pwd='+password+';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 
 # Setting the working env
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
@@ -173,4 +173,4 @@ def adminlogout():
 
 if __name__ == '__main__':
     app.secret_key=os.environ['KEY']
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
