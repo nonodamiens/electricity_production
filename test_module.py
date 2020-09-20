288fca2ffb36cd422171780b3b608b5a106f478f
arnaud <arnaud.mercier@orange.com>
2020-09-20 18:45:06 +0200

    testing csv to pandas

--- test_module.py
+++ test_module.py
@@ -4,8 +4,15 @@ import pandas as pd
 
 def csv_upload(csv_file):
     ''' Method for uploading csv file to database
+
+    The csv file must be separated with ';'
+
+    arguments:
+    - csv_file: links to the file (string)
     '''
-    df = pd.read_csv(csv_file)
-    return df
+    
+    df = pd.read_csv(csv_file, sep=';')
+    
+    return df.groupby('Date').sum()
 
 print(csv_upload('./csv_files/test.csv'))
