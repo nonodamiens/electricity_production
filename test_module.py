705d82b96eb912a4b5e78fda5eb4ece8c871ce51
arnaud <arnaud.mercier@orange.com>
2020-09-20 20:12:04 +0200

    test dataframe electricity old data construction

--- test_module.py
+++ test_module.py
@@ -10,9 +10,22 @@ def csv_upload(csv_file):
     arguments:
     - csv_file: links to the file (string)
     '''
+
+    dataframe = pd.read_csv(csv_file, sep=';')
     
-    df = pd.read_csv(csv_file, sep=';')
+    dataframe_columns_list = dataframe.columns.tolist()
+    mandatory_columns_list = ['Consommation', 'Prévision J', 'Fioul',\
+         'Charbon', 'Gaz', 'Nucléaire', 'Eolien', 'Solaire', 'Hydraulique',\
+              'Pompage', 'Bioénergies', 'Ech. physiques', 'Taux de Co2']
+
+    # Validity check
+    for column_name in mandatory_columns_list:
+        if column_name not in dataframe_columns_list:
+            return 'error, csv file not conform to database'
     
-    return df.groupby('Date').sum()
+    dataframe_by_day = dataframe.groupby('Date').sum()
+    dataframe_by_day = dataframe_by_day[mandatory_columns_list]
+
+    return dataframe_by_day
 
 print(csv_upload('./csv_files/test.csv'))
